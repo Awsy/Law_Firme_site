8f89b40a948f6212884081a61fb1b846c0f0d368
Awsy <bass_master89@mail.ru>
2017-09-16 23:08:23 +0300

    v4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express= require("express");
+var express = require("express");
 var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser')
@@ -11,62 +11,55 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var routes = {
-    "/" : "index",
-    "/about":"about",
-    "/faq":"faq",
-    "/catalogue":"catalogue",
-    "/contact":"contact",
-    "/catalogue/:id":"catalog_template"
+    "/": "index",
+    "/about": "about",
+    "/faq": "faq",
+    "/catalogue": "catalogue",
+    "/contact": "contact",
+    "/catalogue/:id": "catalog_template"
 }
 
-for(let key in routes){
+for (let key in routes) {
 
 
-    if(routes[key]=="faq"){
+    if (routes[key] == "faq") {
 
-        app.get(key,function(req, res){
-          var b = fs.readFileSync(__dirname+'/views/faqs.json');
-          var b = JSON.parse(b.toString());
-            res.render(__dirname+ "/views/" + routes[key], {title: "Вопросы-Ответы", obj:b})
-        });
-    }
-
-    else if(routes[key]=="catalogue"){
-
-        app.get(key,function(req, res){
-
-            var b = fs.readFileSync(__dirname+'/views/service.json');
+        app.get(key, function(req, res) {
+            var b = fs.readFileSync(__dirname + '/views/faqs.json');
             var b = JSON.parse(b.toString());
-            res.render(__dirname+ "/views/" + routes[key], {title: "Услуги", obj:b})
+            res.render(__dirname + "/views/" + routes[key], { title: "Вопросы-Ответы", obj: b })
+        });
+    } else if (routes[key] == "catalogue") {
+
+        app.get(key, function(req, res) {
+
+            var b = fs.readFileSync(__dirname + '/views/service.json');
+            var b = JSON.parse(b.toString());
+            res.render(__dirname + "/views/" + routes[key], { title: "Услуги", obj: b })
         });
 
-    }
-    else if(routes[key]=="catalog_template"){
+    } else if (routes[key] == "catalog_template") {
 
-        app.get('/catalogue/:id',function(req, res){
+        app.get('/catalogue/:id', function(req, res) {
             console.log(req.params.id);
 
 
-            var b = fs.readFileSync(__dirname+'/views/service.json');
+            var b = fs.readFileSync(__dirname + '/views/service.json');
             b = JSON.parse(b.toString());
             b = b[req.params.id];
 
 
-            res.render(__dirname+ "/views/" + routes[key], {title: "Услуги", obj:b})
+            res.render(__dirname + "/views/" + routes[key], { title: "Услуги", obj: b })
         });
-    }
+    } else {
 
-    else{
-
-        app.get(key,function(req, res){
+        app.get(key, function(req, res) {
             console.log(routes[key])
-            res.render(__dirname+ "/views/" + routes[key])
+            res.render(__dirname + "/views/" + routes[key])
         });
     }
-
-
 
 
 }
 
-app.listen(3000);
+app.listen(3000)
