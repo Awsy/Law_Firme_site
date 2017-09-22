4fef7da1364d8679e97fe79a07670f457f359c15
Awsy <bass_master89@mail.ru>
2017-09-22 11:03:59 +0300

    all changes have been fixed

--- public/js/js.js
+++ public/js/js.js
@@ -1,18 +1,16 @@
 
 
 $( function() {
-    // run the currently selected effect
+
     function runEffect() {
  
-      // Most effect types need no options passed by default
+
       var options = {};
-      // some effects have required parameters
- 
-      // Run the effect
+
+
       $( ".effect" ).show( "fold", options, 500 );
     };
- 
-    // Set effect from select menu value
+
     $( document ).on( "scroll", function(e) {
         console.log(document.body.scrollTop)
 
